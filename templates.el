9b105668da8e15fdb48e14f57bba09ee54687ca0
Jonas Reinsch <jonas.reinsch@gmail.com>
2011-07-27 01:13:54 +0200

    add new files

---
+++ templates.el
@@ -0,0 +1,24 @@
+(defun template-insert ()
+  "insert a template if the buffer is empty and if a template corresponding to the file name or file name extension exists"
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (when file-name
+      (let*
+          ((e (or (file-name-extension file-name) (file-name-nondirectory file-name)))
+           (template-file-name (concat "~/templates/template." e)))
+        (when (and
+               (= 0 (buffer-size))
+               (file-readable-p template-file-name))
+          (goto-char (point-min))
+          (insert-file-contents template-file-name)
+          ;; 3rd arg 't: no error, return nil instead when search not successful
+          (let ((e (re-search-forward "[^\s-][^\s-][\s-]*CURSOR_POS" nil 't)))
+            (if e
+                (progn
+                  (delete-region (match-beginning 0) (point)))
+
+;;                  (beginning-of-line)
+;;                  (delete-region (point) e))
+              (goto-char (point-max)))))))))
+
+(add-hook 'find-file-hook 'template-insert)
