48c0a627edee303ed3645e9ca600649e2f6e60b9
Jonas Reinsch <jonas.reinsch@gmail.com>
2011-07-27 01:12:49 +0200

    delete / rename...

--- template_gitignore.el
+++
@@ -1,8 +0,0 @@
-(defun tgi ()
-  "Insert a gitignore Template"
-  (interactive)
-  (goto-char (point-min))
-  (insert-file-contents
-   (expand-file-name "template_gitignore.txt"
-                     (expand-file-name "templates" "~")))
-  (goto-char (point-max)))
